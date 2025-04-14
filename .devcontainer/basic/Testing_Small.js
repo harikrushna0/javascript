817c209d83fe5aed7696c8aa9d02da6a3a896172
hsuhagiya-fintech <harikrushnasuhagiya59@gmail.com>
2025-04-14 13:07:36 +0530

    Update Testing_Small.js

--- .devcontainer/basic/Testing_Small.js
+++ .devcontainer/basic/Testing_Small.js
@@ -4,24 +4,6 @@ const listContainer = document.getElementById("list-container");
 
 
 
-function addTask() {
-    if (inputBox.value === "") {
-        alert("You must write something!");
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        li.classList.add("task");
-        listContainer.appendChild(li);
-
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-
-        // Clear the input box after adding a task
-        inputBox.value = "";
-        saveData();
-    }
-}
 
 listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
