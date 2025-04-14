516ec81d68b4c43225d390ba90b9ffdbd5d488c8
hsuhagiya-fintech <harikrushnasuhagiya59@gmail.com>
2025-04-14 14:16:44 +0530

    Update Testing_Small.js

--- .devcontainer/basic/Testing_Small.js
+++ .devcontainer/basic/Testing_Small.js
@@ -8,16 +8,7 @@ function clearAllTasks() {
     }
 }
 
-listContainer.addEventListener("click", function (e) {
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData();
-    } 
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
+
 
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
